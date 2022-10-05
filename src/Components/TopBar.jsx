6f0b29b1965fa9d442f3fdeee595455b23c70a74
kauaneiras <kauante@hotmail.com>
2022-10-05 12:52:38 -0300

    [SelectMovie] Get Movie From Axios --> OK

---
+++ src/Components/TopBar.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export default function TopBar(){
+    return(
+        <TopBarContainer>
+            <Title>Cineflex</Title>
+        </TopBarContainer>
+    )
+}
+
+const TopBarContainer = styled.div`
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 67px;
+    background-color: #C3CFD9;
+    left: 0px;
+    top: 0px;
+`
+
+const Title = styled.h1`
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 34px;
+    Align: center;
+    color: #E8833A;
+    line-height: 40px;
+    `
--- src/Pages/SelectMovie.jsx
+++ src/Pages/SelectMovie.jsx
@@ -1,7 +1,67 @@
-export default function SelectMovie(){
-    return(
-        <div>
-            <h1>Selecione o filme</h1>
-        </div>
-    )
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+import TopBar from "../Components/TopBar"
+import Loading from "../Components/Loading"
+import GetMovie from "../API/GetMovie"
+
+export default function SelectMovie() {
+
+    const [movies, setMovies] = useState(false);
+
+    useEffect(() => {
+        const promisse = axios.get(GetMovie);
+        promisse.then((sucess) => { setMovies(sucess.data) });
+        promisse.catch((warning) => console.log(warning.response));
+    }, []);
+
+    if (movies === false) {
+        return (<AlignPage><TopBar/><Loading /></AlignPage>)
+    }
+    else {
+        return (
+            <AlignPage>
+                <TopBar />
+                <h2 style={{fontSize: '50px', color: 'black'}}>Selecione o filme</h2>
+                <MoviesAlign>
+                    {movies.map((movie) => <MovieImage><Link to={`/sessoes/${movie.id}`}><img src={movie.posterURL} alt="" /></Link></MovieImage>)}
+                </MoviesAlign>
+            </AlignPage>
+        )
+    }
 }
+
+
+const AlignPage = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    background-color: #FFFFFF;
+    position: absolute;
+    `
+
+const MoviesAlign = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    width: 129px;
+    height: 193px;
+    left: 213px;
+    top: 177px;
+    margin: 10px;
+    
+`
+
+const MovieImage = styled.div`
+    width: 129px;
+    height: 193px;
+    left: 213px;
+    top: 177px;
+    margin: 10px;
+`
